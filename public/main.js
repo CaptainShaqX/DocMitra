dbf404c2e5b6842c947f11fa9f73410bf39f8288
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2024-03-16 22:48:23 +0530

    GPT Fix(Server.js)

--- public/main.js
+++ public/main.js
@@ -1,71 +1,145 @@
-let peer = require('simple-peer');
+// let peer = require('simple-peer');
+// let socket = io();
+// const vider = document.querySelector('video');
+// let client = {};
+
+// //get stream
+// navigator.mediaDevices.getUserMedia( {video : true, audio : true} )
+//     .then(stream => {
+//         socket.emit('NewClient')
+//         video.srcObject = stream;
+//         video.play();
+
+// // Used to initaialize a peer connection
+//         function InitPeer(type){
+//             let peer = new Peer ({initiator: (type == 'init') ? true : false, stream: stream, tricle: false});
+//             peer.on('stream', function(stream) {
+//             CreateVideo(stream)
+//         })
+//         peer.on('close', function(){
+//             document.getElementById('peerVideo').remove();
+//             peer.destroy();
+//         })
+//         return peer;
+//         }
+
+// // FOr peer of type init
+//         function MakePeer(){
+//             client.gotAnswer = false;
+//             let peer = InitPeet('init')
+//             peer.on('signal', function(data){
+//                 if(!client.gotAnswer){
+//                     socket.emit('Offer', data);
+//                 }
+//             })
+//             client.peer = peer;
+//         }
+
+// // FOr peer of Type init
+//         function FrontAnswer(offer){
+//             let peer = InitPeer('notInit');
+//             peer.on('signal', (data) => {
+//                 socket.emit('Answer', data);
+//             })
+//             peer.signal(offer)
+//         }
+
+
+//         function SignalAnswer(answer){
+//             client.gotAnswer = true;
+//             let peer = client.peer
+//             peer.signal(answer)
+//         }
+
+//         function CreativeVideo(stream){
+//             let video = document.createElement('video')
+//             video.id = 'peer.Video';
+//             video.srcObject = stream;
+//             video.class = 'ember-responsive-item';
+//             document.querySelector('#peerDiv').appendChild(video);
+//             video.play();
+//         }
+
+//         function SessionActive(){
+//             document.write('Session Active, Please Come Back Later')
+//         }
+
+
+//     }
+// )
+
+// .catch(err => document.write(err));
+
+
+
+
+
+
+let Peer = require('simple-peer');
+let io = require('socket.io-client');
+const video = document.querySelector('video');
 let socket = io();
-const vider = document.querySelector('video');
+
 let client = {};
 
-//get stream
-navigator.mediaDevices.getUserMedia( {video : true, audio : true} )
+// get stream
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
-        socket.emit('NewClient')
+        socket.emit('NewClient');
         video.srcObject = stream;
         video.play();
 
-// Used to initaialize a peer connection
-        function InitPeer(type){
-            let peer = new Peer ({initiator: (type == 'init') ? true : false, stream: stream, tricle: false});
-            peer.on('stream', function(stream) {
-            CreateVideo(stream)
-        })
-        peer.on('close', function(){
-            document.getElementById('peerVideo').remove();
-            peer.destroy();
-        })
-        return peer;
+        // function to initialize a peer connection
+        function InitPeer(type) {
+            let peer = new Peer({ initiator: (type == 'init'), stream: stream, trickle: false });
+            peer.on('stream', function (stream) {
+                CreateVideo(stream);
+            });
+            peer.on('close', function () {
+                document.getElementById('peerVideo').remove();
+                peer.destroy();
+            });
+            return peer;
         }
 
-// FOr peer of type init
-        function MakePeer(){
+        // for peer of type 'init'
+        function MakePeer() {
             client.gotAnswer = false;
-            let peer = InitPeet('init')
-            peer.on('signal', function(data){
-                if(!client.gotAnswer){
+            let peer = InitPeer('init');
+            peer.on('signal', function (data) {
+                if (!client.gotAnswer) {
                     socket.emit('Offer', data);
                 }
-            })
+            });
             client.peer = peer;
         }
 
-// FOr peer of Type init
-        function FrontAnswer(offer){
+        // for peer of Type 'notInit'
+        function FrontAnswer(offer) {
             let peer = InitPeer('notInit');
             peer.on('signal', (data) => {
                 socket.emit('Answer', data);
-            })
-            peer.signal(offer)
+            });
+            peer.signal(offer);
         }
 
-
-        function SignalAnswer(answer){
+        function SignalAnswer(answer) {
             client.gotAnswer = true;
-            let peer = client.peer
-            peer.signal(answer)
+            let peer = client.peer;
+            peer.signal(answer);
         }
 
-        function CreativeVideo(stream){
-            let video = document.createElement('video')
-            video.id = 'peer.Video';
+        function CreateVideo(stream) {
+            let video = document.createElement('video');
+            video.id = 'peerVideo';
             video.srcObject = stream;
-            video.class = 'ember-responsive-item';
+            video.classList.add('ember-responsive-item');
             document.querySelector('#peerDiv').appendChild(video);
             video.play();
         }
 
-        function SessionActive(){
-            document.write('Session Active, Please Come Back Later')
+        function SessionActive() {
+            document.write('Session Active, Please Come Back Later');
         }
-
-
-    }
-)
-
-.catch(err => document.write(err));
+    })
+    .catch(err => document.write(err));
--- server.js
+++ server.js
@@ -1,5 +1,33 @@
-const express = require('expresss')
-const app = express()
-// app = require('express'){}
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const http = require('http')
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', function(socket) {
+    socket.on('NewClient', function() {
+        if (io.engine.clientsCount === 0) {
+            socket.emit('CreatePeer');
+        }
+    });
+
+    socket.on('Offer', function(offer) {
+        socket.broadcast.emit('BackOffer', offer);
+    });
+
+    socket.on('Answer', function(data) {
+        socket.broadcast.emit('BackAnswer', data);
+    });
+
+    socket.on('disconnect', function() {
+        if (io.engine.clientsCount === 0) {
+            socket.broadcast.emit('Disconnect');
+        }
+    });
+});
+
+server.listen(3000, function() {
+    console.log('listening on *:3000');
+});
