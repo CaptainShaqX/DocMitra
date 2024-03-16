01f982382c9b76ea189179e138229ffadd8a9db7
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2024-03-17 01:12:23 +0530

    It worked (1)

--- server.js
+++ server.js
@@ -1,33 +1,80 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
-io.on('connection', function(socket) {
-    socket.on('NewClient', function() {
-        if (io.engine.clientsCount === 0) {
-            socket.emit('CreatePeer');
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000
+
+
+app.use(express.static(__dirname + "/public"))
+let clients = 0;
+
+io.on('connection', function(socket){
+    socket.on("NewClient", function(){
+        if (clients < 2 ){
+            if(clients == 1){
+                this.emit('CreatePeer')
+            }
         }
-    });
+        else 
+            this.emit('SessionActive')
+        clients++
+    })
+    socket.on('Offer', SendOffer)
+    socket.on('Answer', SendAnswer)
+    socket.on('disconnect', Disconnect)
+})
 
-    socket.on('Offer', function(offer) {
-        socket.broadcast.emit('BackOffer', offer);
-    });
+    function Disconnect (){
+    if (clients > 0)
+    clients--
+    }
 
-    socket.on('Answer', function(data) {
-        socket.broadcast.emit('BackAnswer', data);
-    });
+    function SendOffer (offer) {
+        this.broadcast.emit("BackOffer", offer)
+    }
+    function SendAnswer (data) {
+        this.broadcast.emit("BackAnswer", data)
+    }
 
-    socket.on('disconnect', function() {
-        if (io.engine.clientsCount === 0) {
-            socket.broadcast.emit('Disconnect');
-        }
-    });
-});
+    http.listen(port, () => console.log(`Active on ${port}`))
 
-server.listen(3000, function() {
-    console.log('listening on *:3000');
-});
+
+
+
+
+
+
+
+
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('NewClient', function() {
+//         if (io.engine.clientsCount === 0) {
+//             socket.emit('CreatePeer');
+//         }
+//     });
+
+//     socket.on('Offer', function(offer) {
+//         socket.broadcast.emit('BackOffer', offer);
+//     });
+
+//     socket.on('Answer', function(data) {
+//         socket.broadcast.emit('BackAnswer', data);
+//     });
+
+//     socket.on('disconnect', function() {
+//         if (io.engine.clientsCount === 0) {
+//             socket.broadcast.emit('Disconnect');
+//         }
+//     });
+// });
+
+// server.listen(3000, function() {
+//     console.log('listening on *:3000');
+// });
+
+
